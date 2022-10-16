10fda99b2c6f9e9b71ac69a847ac8e40c38ac11c
Jexica Ayran <jexica@jexica-mac.myfiosgateway.com>
2022-10-16 02:06:52 -0400

    video placeholder

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,5 +1,11 @@
 import React from "react";
+import VideoBg from "./VideoBg";
 
 export default function Layout(props) {
-  return <div className="w-screen h-screen"> {props.children} </div>;
+  return (
+    <div className="w-screen h-screen">
+      {props.children}
+      {/* <VideoBg /> */}
+    </div>
+  );
 }
