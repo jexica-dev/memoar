24e13c8357f30a3c2c70d47c7007e89576aa267c
Jexica Ayran <jexica@jexica-mac.myfiosgateway.com>
2022-10-16 01:35:39 -0400

    adding arrow and bounce animation

--- client/src/components/MemoButton.jsx
+++ client/src/components/MemoButton.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import arrow from "../images/arrow.svg";
 
 export default function MemoButton(props) {
   return (
     <div className="">
+      <div className="w-screen flex flex-col  place-items-center place">
+        <div className=" animate-fadein-03 mb-10">
+          <img
+            className=" animate-bounce-slow  place-items-center "
+            src={arrow}
+            alt=""
+            width="100px"
+            srcset=""
+          />
+        </div>
+      </div>
       <a href="https://interactiveknowledge.8thwall.app/vps-bespoke">
         <div className="font-chillax text-7xl lg:text-[200px] md:text-9xl sm:text-7xl animate-fadein-02 hover:text-stone-700 ">
           MemoAR
