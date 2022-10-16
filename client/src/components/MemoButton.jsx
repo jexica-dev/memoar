56f81ff0ddd52961956c53e745a3960b68164af6
Jexica Ayran <jexica@jexica-mac.myfiosgateway.com>
2022-10-16 01:08:15 -0400

    adding link to MemoAR

--- client/src/components/MemoButton.jsx
+++ client/src/components/MemoButton.jsx
@@ -4,13 +4,13 @@ import { Link } from "react-router-dom";
 export default function MemoButton(props) {
   return (
     <div className="">
-      <Link to="">
-        <div className="font-chillax text-9xl animate-fadein-02 hover:text-stone-700 ">
+      <a href="https://interactiveknowledge.8thwall.app/vps-bespoke">
+        <div className="font-chillax text-7xl lg:text-[200px] md:text-9xl sm:text-7xl animate-fadein-02 hover:text-stone-700 ">
           MemoAR
         </div>
-      </Link>
+      </a>
       <p className=" font-clash text-3xl animate-fadein-03">
-        create memo, leave a memory...
+        create a memo, leave a memory...
       </p>
     </div>
   );
